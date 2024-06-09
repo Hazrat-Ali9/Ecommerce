2ba085498753776529687f8bcb2b8fb19cc5c9f5
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-06-09 22:36:25 +0600

    Trading Product :tshirt:

--- src/Components/Product/Trading.js
+++ src/Components/Product/Trading.js
@@ -4,7 +4,7 @@ import NewDetails from './Details/NewDetails';
 
 const Trading = () => {
     const [products] = useProduct();
-
+// Trading
   return (
     <div className="mx-5">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-12 gap-y-6">
